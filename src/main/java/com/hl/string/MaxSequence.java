77ffc78fbfcee55ed58f04c891bef7583389397a
huanglong6 <huanglong6@jd.com>
2018-12-13 10:52:28 +0800

    初始提交

---
+++ src/main/java/com/hl/string/MaxSequence.java
@@ -0,0 +1,14 @@
+package com.hl.string;
+
+/**
+ * @author huanglong
+ * @Description: 最大子序列
+ * @date 2018/12/1310:37
+ */
+public class MaxSequence {
+    /* 比如{5,-3,4,2}的最大子序列就是 {5,-3,4,2}，它的和是8,达到最大；而 {5,-6,4,2}的最大子序列是{4,2}，它的和是6 */
+    /* ，只要前i项的和还没有小于0那么子序列就一直向后扩展，否则丢弃之前的子序列开始新的子序列 */
+    public void maxSequence(String[] arr){
+
+    }
+}
